d1dd3b98103f5473f467a8ce5b2586ac8c0329d5
akash100a <arakash968786@gmail.com>
2024-11-23 02:03:41 +0600

    up

--- src/component/layers/Navbar.jsx
+++ src/component/layers/Navbar.jsx
@@ -4,12 +4,12 @@ import MenuLi from './MenuLi'
 const Navbar = () => {
   return (
     <ul className="menu mt-[118px] flex flex-col gap-y-[30px]">
-      <MenuLi litext='Dashboard' />
-      <MenuLi highlight='opacity-1' litext='Expenses' to='/expenses'/>
-      <MenuLi litext='Wallets' to='/wallets'/>
-      <MenuLi litext='Summary' />
-      <MenuLi litext='Accounts' />
-      <MenuLi litext='Settings' />
+      <MenuLi litext='Dashboard' to='dashboard' />
+      <MenuLi highlight='opacity-1' litext='Expenses' to='/expenses' />
+      <MenuLi litext='Wallets' to='/wallets' />
+      <MenuLi litext='Summary' to='/summary' />
+      <MenuLi litext='Accounts' to='/accounts' />
+      <MenuLi litext='Settings' to='/setting' />
     </ul>
   )
 }
