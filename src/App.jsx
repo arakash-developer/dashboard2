a1e2f3cef543f4ea2f81fe66c9decef6ec1e6b6d
akash100a <arakash968786@gmail.com>
2024-11-24 02:30:27 +0600

    up

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,6 @@ import About from './component/pages/About'
 import Expenses from './component/pages/Expenses';
 import Error from './component/layers/Error';
 import React, { lazy, useEffect, useLayoutEffect, useState } from 'react'
-import Container from './component/layers/Container'
-import Profile from '../public/Profile.jpg'
-import Navbar from "./component/layers/Navbar";
-import PulseLoader from "react-spinners/PulseLoader";
 
 
 // const wait = (time)=>{
@@ -39,41 +35,8 @@ const router = createBrowserRouter(
 
 
 const App = () => {
-  let [loading, setLoading] = useState(true);
-  useLayoutEffect(() => {
-    setTimeout(() => {
-      setLoading(false)
-    }, 1000);
-  },[])
   return (
-    <div>
-      <Container className='h-screen py-10'>
-        <div className="flex rounded-[30px] w-full h-full bg-[#101010] pl-[80px] py-5 pr-5 text-[#fff]">
-          <div className="mt-[60px] mr-[84px]">
-            <div className="w-16 h-16 relative">
-              <div className="rounded-lg overflow-hidden">
-                <img src={Profile} alt={Profile} />
-              </div>
-              <div className="w-[29px] h-[29px] flex justify-center items-center absolute -right-[15px] z-[999] -top-[15px] rounded-full bg-[#DC3434]">
-                <p className='font-semibold text-[0.81rem] text-[#fff]'>1</p>
-              </div>
-            </div>
-            <h1 className='mt-5 mb-1 font-semibold text-3xl leading-[117%] text-[#fff]'>Samantha</h1>
-            <p className='font-normal text-[1.06rem] leading-[159%] text-[#fff] opacity-60'>samantha@email.com</p>
-            <Navbar />
-          </div>
-          <div className="my w-[1060px] rounded-[30px] overflow-hidden bg-[#FFFFFF]">
-            {
-              loading ?
-                <div className="w-full h-full bg-[#fff] flex justify-center items-center">
-                  <PulseLoader speedMultiplier={1.5} color="#101010" />
-                </div> :
-                <RouterProvider router={router} />
-            }
-          </div>
-        </div>
-      </Container>
-    </div>
+    <RouterProvider router={router} />
   )
 }
 export default App
--- src/component/layers/Root.jsx
+++ src/component/layers/Root.jsx
@@ -1,15 +1,38 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useEffect, useState } from 'react'
 import { Outlet } from 'react-router-dom'
-import Navbar from './Navbar'
-import Footer from './Footer'
+import Container from './Container';
+import Profile from '../../../public/Profile.jpg'
+import Navbar from "../layers/Navbar";
 import PulseLoader from "react-spinners/PulseLoader";
-
 
 
 const Root = () => {
     return (
         <>
-            <Outlet />
+            <div>
+                <Container className='h-screen py-10'>
+                    <div className="flex rounded-[30px] w-full h-full bg-[#101010] pl-[80px] py-5 pr-5 text-[#fff]">
+                        <div className="mt-[60px] mr-[84px]">
+                            <div className="w-16 h-16 relative">
+                                <div className="rounded-lg overflow-hidden">
+                                    <img src={Profile} alt={Profile} />
+                                </div>
+                                <div className="w-[29px] h-[29px] flex justify-center items-center absolute -right-[15px] z-[999] -top-[15px] rounded-full bg-[#DC3434]">
+                                    <p className='font-semibold text-[0.81rem] text-[#fff]'>1</p>
+                                </div>
+                            </div>
+                            <h1 className='mt-5 mb-1 font-semibold text-3xl leading-[117%] text-[#fff]'>Samantha</h1>
+                            <p className='font-normal text-[1.06rem] leading-[159%] text-[#fff] opacity-60'>samantha@email.com</p>
+                            <Navbar />
+                        </div>
+                        <div className="my w-[1060px] rounded-[30px] overflow-hidden bg-[#FFFFFF]">
+                            <Suspense fallback={<PulseLoader />}>
+                                <Outlet />
+                            </Suspense>
+                        </div>
+                    </div>
+                </Container>
+            </div>
         </>
     )
 }
